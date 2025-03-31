73ab51cb26945dde581f5b597f7b21608122db4e
Sanjoy Kumar Maity <sanjoykumarmaity05@gmail.com>
2025-03-31 11:09:54 +0530

    Add lots of controller and router file

--- src/app.js
+++ src/app.js
@@ -24,8 +24,6 @@ import userRouter from './routes/user.routes.js'
 //routes declaration
 app.use("/api/v1/users",userRouter)
 
-//http://localhost:8000/api/v1/users/register
-//http://localhost:8000/api/v1/users/login
 
 export { app } 
 
